69641e861c38aee255affd37cb5a50f502247883
Parche <93171758+ParcheEnGit@users.noreply.github.com>
2026-04-10 05:37:15 -0400

    Agregar RequestTimingLogger al middleware para registrar el tiempo de procesamiento de las solicitudes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use App\Http\Middleware\RequestTimingLogger;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,6 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
                 ? null
                 : $frontendLoginUrl
         );
+
+        $middleware->append(RequestTimingLogger::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
