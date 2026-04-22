ded44f0ffa9a4e099539aaf173bc728d3c2ee624
mikory1 <78696618+mikory1@users.noreply.github.com>
2026-04-22 16:32:41 +0000

    bug validar

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -43,7 +43,7 @@ class AuthController extends Controller
     {
         $credentials = $request->validate([
             'email' => ['required', 'email'],
-            'password' => ['required', 'string', 'regex:/^\S+$/'],
+            'password' => ['required', 'string', 'min:8'],
         ]);
 
         /** @var User|null $user */
@@ -192,9 +192,10 @@ class AuthController extends Controller
         DB::table('Usuario')->insert([
             'nombre_completo' => $user->name,
             'correo' => $user->email,
-            'contraseña_hash' => $user->password,
             'estado_perfil' => 'activo',
             'visibilidad_perfil' => 'publico',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 
