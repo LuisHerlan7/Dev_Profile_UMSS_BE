175fc3e334f5c16cc6400e9e50d6a94890d84241
ParcheEnGit <adrianpereztapia@gmail.com>
2026-06-14 22:45:28 -0700

    fix: update root route to return index file without .html extension

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
-    return response()->file(public_path('index.html'));
+    return response()->file(public_path('index'));
 });
 
 Route::post('/register', [RegisterController::class, 'register']);
