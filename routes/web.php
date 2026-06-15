5c13f967173b3c457a13fc8eb032b1879d93adca
ParcheEnGit <adrianpereztapia@gmail.com>
2026-06-14 22:50:02 -0700

    fix: update route to return view for index instead of file

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
+<?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return response()->file(public_path('index'));
-});
+Route::get('/{any}', function () {
+    return view('index');
+})->where('any', '.*');
 
 Route::post('/register', [RegisterController::class, 'register']);
