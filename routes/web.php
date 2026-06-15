fac45d0e0a1bf35664453df626e12e733cc5a101
ParcheEnGit <adrianpereztapia@gmail.com>
2026-06-14 23:40:26 -0700

    fix: update route to return a simple response for the root path

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RegisterController;
 
-Route::get('/{any}', function () {
-    return view('index');
-})->where('any', '.*');
-
-Route::post('/register', [RegisterController::class, 'register']);
+Route::get('/', function () {
+    return 'FUNCIONA';
+});
